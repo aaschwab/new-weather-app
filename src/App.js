17280efbaeb3129208429ab3aaa10054be1280a2
aaschwab <aries7020@gmail.com>
2024-03-31 19:30:15 -0500

    added Github to project

--- src/App.js
+++ src/App.js
@@ -2,7 +2,17 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        This project is coded by Amanda Schwab and is open-sourced on{" "}
+        <a href="https://github.com/aaschwab/new-weather-app" target="blank">
+          Github
+        </a>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
